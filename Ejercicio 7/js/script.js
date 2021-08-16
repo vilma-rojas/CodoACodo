7c2c006cb3128ad25892e87fcdccf61dc101d6c7
vilma-rojas <vilma_diana@outlook.com>
2021-08-16 18:29:21 -0300

    tercer ejercicio javascript

---
+++ Ejercicio 7/js/script.js
@@ -0,0 +1,21 @@
+function ingresarNota() {
+    var nota = prompt("Ingresar Nota");
+    return nota;
+}
+
+function nombreUsuario(){
+    var nombre = prompt("Ingresar Nombre");
+    return nombre;
+}
+
+function maximoDe3Notas(){
+    var nota1 = ingresarNota();
+    var usuario1 = nombreUsuario();
+   /* var notas = [ingresarNota(), ingresarNota(), ingresarNota()];
+    var usuarios = [nombre]
+    for (var i; i<notas.length(); i++){
+        var b = notas[i] > notas[i++];
+    }*/
+
+}
+const result = prompt("How old are you?", 18);
